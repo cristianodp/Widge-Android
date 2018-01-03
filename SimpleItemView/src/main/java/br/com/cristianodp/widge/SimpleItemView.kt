d26175c75938b9321e48987ce5f8d9fc6127a537
Cristiano Dini <cristianodp@gmail.com>
2018-01-03 00:19:00 -0200

    Add attr color, font size and styles

--- SimpleItemView/src/main/java/br/com/cristianodp/widge/SimpleItemView.kt
+++ SimpleItemView/src/main/java/br/com/cristianodp/widge/SimpleItemView.kt
@@ -1,8 +1,10 @@
 package br.com.cristianodp.widge
 
+import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.Context
 import android.os.Build
+import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.View
 import android.widget.ImageView
@@ -14,9 +16,9 @@ import android.widget.TextView
  */
 class SimpleItemView : LinearLayout {
 
-    //Text views
+    private lateinit var mConstraintLayoutContainer: ConstraintLayout
     private lateinit var mIcon: ImageView
-    private lateinit var mTitle: TextView
+    private lateinit var mLabel: TextView
     private lateinit var mValue: TextView
 
 
@@ -34,26 +36,82 @@ class SimpleItemView : LinearLayout {
     /**
      * Initialize view
      */
+    @SuppressLint("Recycle")
     private fun init(attrs: AttributeSet?) {
-
-        View.inflate(context, R.layout.simple_item_view_layout, this)
-
-        //Get references to text views
-        mIcon = findViewById<ImageView>(R.id.ImageViewIcon)
-        mTitle = findViewById<TextView>(R.id.TextViewLabel)
-        mValue = findViewById<TextView>(R.id.TextViewValue)
 
         val customAttrs = context.obtainStyledAttributes(
                 attrs,
                 R.styleable.SimpleItemView)
 
-        var icon = customAttrs.getDrawable(R.styleable.SimpleItemView_icon)
-        var label = customAttrs.getString(R.styleable.SimpleItemView_label)
-        var value = customAttrs.getString(R.styleable.SimpleItemView_value)
+        val icon = customAttrs.getDrawable(R.styleable.SimpleItemView_icon)
+        val label = customAttrs.getString(R.styleable.SimpleItemView_label)
+        val value = customAttrs.getString(R.styleable.SimpleItemView_value)
+        val size = customAttrs.getDimension(R.styleable.SimpleItemView_textSize,0f)
+        val color = customAttrs.getColor(R.styleable.SimpleItemView_textColor,0)
+        val sizeLabel = customAttrs.getDimension(R.styleable.SimpleItemView_textSizeLabel,0f)
+        val colorLabel = customAttrs.getColor(R.styleable.SimpleItemView_textColorLabel,0)
+        val sizeValue = customAttrs.getDimension(R.styleable.SimpleItemView_textSizeValue,0f)
+        val colorValue = customAttrs.getColor(R.styleable.SimpleItemView_textColorValue,0)
+        val backgroundColor = customAttrs.getColor(R.styleable.SimpleItemView_backgroundColor,0)
+        val style = customAttrs.getInt(R.styleable.SimpleItemView_style,0)
 
+        var view_layout = R.layout.simple_item_view_layout
+        when (style) {
+            0 -> view_layout = R.layout.simple_item_view_layout
+            1 -> view_layout = R.layout.simple_item_center_view_layout
+            else -> { // Note the block
+                view_layout = R.layout.simple_item_view_layout
+            }
+        }
+
+        View.inflate(context, view_layout,this)
+
+        //Get references to text views
+        mIcon = findViewById<ImageView>(R.id.ImageViewIcon)
+        mLabel = findViewById<TextView>(R.id.TextViewLabel)
+        mValue = findViewById<TextView>(R.id.TextViewValue)
+        mConstraintLayoutContainer = findViewById<ConstraintLayout>(R.id.ConstraintLayoutContainer)
 
         mIcon.setImageDrawable(icon)
-        mTitle.text = label
+        mLabel.text = label
         mValue.text = value
+
+        if (size != 0f ){
+            mLabel.textSize = size
+            mValue.textSize = size
+
+        }
+
+        if (color != 0 ){
+            mLabel.setTextColor(color)
+            mValue.setTextColor(color)
+        }
+
+
+        if (sizeValue != 0f ){
+            mValue.textSize = sizeValue
+        }
+
+        if (colorValue != 0 ){
+            mValue.setTextColor(colorValue)
+        }
+
+        if (sizeLabel != 0f ){
+            mLabel.textSize = sizeLabel
+        }
+
+        if (colorLabel != 0 ){
+            mLabel.setTextColor(colorLabel)
+        }
+
+
+        if (backgroundColor != 0 ) {
+            mConstraintLayoutContainer.setBackgroundColor(backgroundColor)
+        }
+
+
+
     }
+
+
 }
